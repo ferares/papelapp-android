5ad3ddd4dd0afb270cd1a00ac0ad0056f3dde144
ferares <fermin@ares.uy>
2024-08-02 22:12:10 -0300

    init

---
+++ app/src/main/java/uy/ares/papelapp/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package uy.ares.papelapp.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
