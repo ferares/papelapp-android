f5a51490dcd774071795be8b8b197a1204fc5d1a
Ferares <fermin@ares.uy>
2026-08-12 10:20:01 -0300

    Fix app links not opening the app but the settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "uy.ares.papelapp.twa"
         minSdk = 23
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.0.0.4"
+        versionCode = 6
+        versionName = "1.0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
