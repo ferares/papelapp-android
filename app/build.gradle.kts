e566122cb0b0d7d20ff85638af65e26a1819fd31
Ferares <fermin@ares.uy>
2026-08-12 10:21:43 -0300

    Update release number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "uy.ares.papelapp.twa"
         minSdk = 23
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.0.0.5"
+        versionCode = 7
+        versionName = "1.0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
